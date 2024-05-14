0052abdf3d2b38c34d012faa1118dd0d49855940
Jimin <sjm1254245781@163.com>
2024-05-14 14:37:30 +0800

    feat: Add a button for negative film

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import React, { ChangeEvent, useEffect, useRef, useState } from "react";
+import { Input, Button } from 'antd';
 
 export default function App() {
-  const [image, setImage] = useState<HTMLImageElement>()
+  const [image, setImage] = useState<HTMLImageElement>();
   const [offset, setOffset] = useState<{
     dx: number;
     dy: number;
   }>({
     dx: 0,
     dy: 0,
-  })
+  });
   const [scale, setScale] = useState(1);
 
   const dragStart = useRef<{
@@ -26,18 +27,18 @@ export default function App() {
     img.onload = () => {
       setImage(img);
     };
-  }
+  };
 
   const onMouseDown = (e: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
     dragStart.current = {
       x: e.clientX - offset.dx,
       y: e.clientY - offset.dy,
     };
-  }
+  };
 
   const onMouseUp = (_e: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
     dragStart.current = null;
-  }
+  };
 
   const onMouseMove = (e: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
     if (dragStart.current) {
@@ -46,17 +47,48 @@ export default function App() {
         dy: e.clientY - dragStart.current.y,
       })
     }
-  }
+  };
 
   const onWheel = (e: React.WheelEvent<HTMLCanvasElement>) => {
-    //const canvas = document.getElementById('canvas') as HTMLCanvasElement;
+    const canvas = document.getElementById('canvas') as HTMLCanvasElement;
     const scaleRange = [0.1, 5];
     let newScale = scale + (e.deltaY < 0 ? 0.5 : (scale < 1.2 ? -0.2 : -0.5));
     if (newScale < scaleRange[0]) newScale = scaleRange[0];
     if (newScale > scaleRange[1]) newScale = scaleRange[1];
+
+    const rect = canvas.getBoundingClientRect();
+    const mouseX = e.clientX - rect.left;
+    const mouseY = e.clientY - rect.top;
+
+    const ratio = newScale / scale;
+    const newDx = mouseX - ratio * (mouseX - offset.dx);
+    const newDy = mouseY - ratio * (mouseY - offset.dy);
+
     setScale(newScale);
-    //const rect = canvas.getBoundingClientRect();
-  }
+    setOffset({
+      dx: newDx, 
+      dy: newDy 
+    });
+  };
+
+  const onNegativeFilm = () => {
+    const canvas = document.getElementById('canvas') as HTMLCanvasElement;
+    const ctx = canvas.getContext('2d');
+
+    if (!ctx) {
+      throw new Error('Can not create canvas context')
+    } else if (image) {
+      const imageData = ctx.getImageData(0, 0, image.width, image.height);
+      const data = imageData.data;
+      for (let i = 0; i < data.length; i += 4) {
+        data[i] = 255 - data[i];
+        data[i + 1] = 255 - data[i + 1];
+        data[i + 2] = 255 - data[i + 2];
+      }
+      const newData = new ImageData(new Uint8ClampedArray(data), imageData.width, imageData.height);
+      ctx.putImageData(newData, 0, 0);
+    }
+  };
   
   useEffect(() => {
     if (!image) {
@@ -81,18 +113,21 @@ export default function App() {
   
   return (
     <div style={{
-      display: 'flex',
+      display: 'inline-flex',
       justifyContent: 'center',
       padding: 20
     }}>
-      <input type="file" accept="image/png, image/jpg, image/jpeg" onChange={onFileUpload}></input>
+      <Input type="file" accept="image/png, image/jpg, image/jpeg" onChange={onFileUpload}></Input>
       <canvas id="canvas" width={512} height={512} style={{
-        border: '1px solid black'
+        border: '1px solid black',
+        marginLeft: 20,
+        marginRight: 20,
       }}
       onMouseDown={onMouseDown}
       onMouseMove={onMouseMove}
       onMouseUp={onMouseUp}
       onWheel={onWheel}/>
+      <Button onClick={onNegativeFilm}>负片</Button>
     </div>
   )
 }
